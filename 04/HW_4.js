f9846104022b23d84e781cc8534fab2544040d8c
Євген Тума <tumagroup002@gmail.com>
2024-05-27 19:08:44 +0200

    04

---
+++ 04/HW_4.js
@@ -0,0 +1,138 @@
+// 1
+// Створіть функцію wellcomUser, яка приймає масив і функцію-колбек hiUser яка виводить в console ('Вітаю ${user}'), і викликає цю функцію для кожного елемента масиву.
+
+
+// ВИБАЧТЕ ЗІ СТРІЛКОВОЮ ФУНКЦІЄЮ ПОКИ ТЯЖКУВАТО. ДА І З ЗВИЧАЙНИМИ ФУНКЦІЯМИ ТЯЖКУВАТО. НЕ МОЖУ ЗРОЗУМІТИ ЛОГІКИ ЩО ДЛЯ ЧОГО РОБИТЬСЯ. МОЖЛИВО НА УРОЦІ ЩЕ РОЗГЛЯНЕМО? HTML/SCC ВСЕ ЗРОЗУМІЛО БО НАГЛЯДНО ВИДНО ЯКИЙ КОД ТИ ПИШЕШ І ЩО ВІН РОБИТЬ.
+
+
+// let users = ['Tom','Max','July','Bred','Anna'];
+// function wellcomUser(users, hiUser){
+//     for(let i=0; i<users.length; i++){
+//         hiUser(users[i]);
+//     }
+// }
+// function hiUser(user){
+//     console.log (`Вітаю ${user}`);
+// }
+// wellcomUser(users, hiUser);
+
+
+
+
+
+
+// push:
+// Завдання 1:
+//  Створіть порожній масив та використайте метод push, щоб додати до нього кілька елементів (наприклад, чисел чи рядків).
+
+
+// let autoName = [];
+// autoName.push('BMW','AUDI','KIA','MERSEDES','FIAT','ZAZ','CITROEN','PEGOUT');
+// autoName.push('OPEL');
+// console.log(autoName)
+
+
+
+
+
+
+// Завдання 2: 
+// Створіть функцію, яка приймає масив і елемент, і використовує push для додавання цього елемента до масиву.
+
+
+// ОТ ТУТ Я НЕ МОЖУ ЗРОЗУМІТИ ДЛЯ ЧОГО ТУТ ФУНКЦІЯ ЯКЩО МОЖНА БЕЗ НЕЇ ВИДАЛИТИ З МАСИВУ ЕЛЕМЕНТ? І ЧЕРЕЗ ЦЕ НЕ МОЖУ ЗРОЗУМІТИ ЯК ЦЕ ЗРОБИТИ.
+
+// let numberValueArr = [];
+// function numberValue(array, element){
+//     array.push(element);
+// }
+
+// console.log(numberValueArr);
+
+
+
+
+
+
+
+// pop:
+// Завдання 1: 
+// Створіть масив і використайте метод pop, щоб видалити останній елемент з масиву.
+
+
+
+// let autoName = [];
+// autoName.push('BMW','AUDI','KIA','MERSEDES','FIAT','ZAZ','CITROEN','PEGOUT');
+// autoName.push('OPEL');
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+// autoName.pop();
+// console.log(autoName);
+
+
+
+// Завдання 2: 
+// Напишіть функцію, яка приймає масив і використовує pop для видалення останнього елемента масиву.
+
+// ОТ ТУТ Я НЕ МОЖУ ЗРОЗУМІТИ ДЛЯ ЧОГО ТУТ ФУНКЦІЯ ЯКЩО МОЖНА БЕЗ НЕЇ ВИДАЛИТИ З МАСИВУ ЕЛЕМЕНТ? І ЧЕРЕЗ ЦЕ НЕ МОЖУ ЗРОЗУМІТИ ЯК ЦЕ ЗРОБИТИ.
+
+// let planetName = ['Earth','Pluton','Mars','Neptun','Jupiter',];
+// function planetMinus(array){
+//     array.pop();
+// }
+// planetMinus();
+// console.log(planetMinus);
+
+
+
+
+
+// unshift:
+// Завдання 1: 
+// Створіть масив і використайте метод unshift, щоб додати елементи в початок масиву.
+
+
+
+// let autoName = ['BMW','AUDI','KIA','MERSEDES','FIAT','ZAZ','CITROEN','PEGOUT'];
+// autoName.unshift('ALFA ROMEO');
+// autoName.unshift(123);
+// console.log(autoName);
+
+
+// З ЗАВДАННЯМ 2 ВСЕ ТЕЖ САМЕ. ВИБАЧТЕ Я ВЖЕ І В ЮТУБІ ДИВИВСЯ ВІДЕО ВІД ІНШИХ УЧИТЕЛІВ І ВСЕ ОДНО НІЧОГО НЕ МОЖУ ЗРОЗУМІТИ.
+
+
+
+// shift:
+// Завдання 1: 
+// Створіть масив і використайте метод shift, щоб видалити перший елемент з масиву.
+
+
+// let autoName = ['BMW','AUDI','KIA','MERSEDES','FIAT','ZAZ','CITROEN','PEGOUT'];
+// autoName.shift();
+// autoName.shift();
+// console.log(autoName);
+
+
+
+
+
+
+
+// fill:
+// Завдання 1: 
+// Створіть масив певного розміру і використайте метод fill, щоб заповнити його певним значенням.
+
+
+// let numberArr = [1,2,3,4,5,6,7,8,9];
+// numberArr.fill('Error');
+// console.log(numberArr);
